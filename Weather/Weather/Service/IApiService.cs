918b71ba57e32b6f8261191ff1855f8e079fb291
Evelyncn1205 <evelynrx_rj@hotmail.com>
2023-10-21 18:54:49 +0100

    Login page Folder Service Folder Models

---
+++ Weather/Weather/Service/IApiService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Models;
+
+namespace Weather.Service
+{
+    public interface IApiService
+    {
+        Task<Response> GetListAsync<T>(string urlBase, string servicePrefix, string controller);
+    }
+}
---
+++ Weather/Weather/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,90 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Weather.ViewModels
+{
+    public class LoginPageViewModel : ViewModelBase
+    {
+       
+        private string _password;
+        private bool _isRunning;
+        private bool _isEnable;
+        private DelegateCommand _loginCommand;
+
+        public LoginPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Login";
+            IsEnable = true;
+
+        }
+
+        public DelegateCommand LoginCommand => _loginCommand ?? (_loginCommand = new DelegateCommand(Login));
+
+
+        public string Email { get; set; }
+
+        public string Password
+        {
+            get => _password;
+
+            set => SetProperty(ref _password, value);
+        }
+
+        public bool isRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+        public bool IsEnable
+        {
+            get => _isEnable;
+            set => SetProperty(ref _isEnable, value);
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter an email.", "Accept");
+                Password = string.Empty;
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter a password.", "Accept");
+                Password = string.Empty;
+                return;
+            }
+
+            //if(!this.Email.Equals("clara_dias@yopmail.com") || !this.Password.Equals("123456"))
+            //{
+            //    await Application.Current.MainPage.DisplayAlert(
+            //        "Error",
+            //        "User or password wrong",
+            //        "Accept");
+            //}
+            if (this.Email.Equals(Email) && this.Password.Equals("123456"))
+            {
+                // Exibir alerta de login bem-sucedido
+                await Application.Current.MainPage.DisplayAlert("Ok", "Login successful!!", "Accept");
+
+                //// Navegação para a página de livro
+                //await NavigationService.NavigateAsync
+                //($"/{nameof(WorldLibraryMasterDetailPage)}/NavigationPage/{nameof(BooksPage)}");
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "User or password wrong",
+                    "Accept");
+            }
+        }
+    }
+}
